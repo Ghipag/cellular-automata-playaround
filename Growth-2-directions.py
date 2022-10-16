ef5ba9a53050f3d9f325c0675abe9ee69e6b93f2
Louis Timperley <68285048+louistimperley99@users.noreply.github.com>
2022-10-16 17:49:50 +0100

    chaotic 2d growth

---
+++ Growth-2-directions.py
@@ -0,0 +1,49 @@
+import cellpylib as cpl
+
+# initial point
+cellular_automaton = cpl.init_simple2d(480, 480)
+cellular_automaton[:, [60], [60]] = 1
+
+# Rule Table
+#(center,north,east,south,west)
+ctrbl_rule = cpl.CTRBLRule(rule_table={
+    (1, 0, 0, 0, 0): 1,
+    (0, 1, 0, 0, 0): 0,
+    (0, 0, 1, 0, 0): 0,
+    (0, 0, 0, 1, 0): 1,
+    (0, 0, 0, 0, 1): 1,
+    (0, 0, 0, 0, 0): 0,
+    (1, 0, 1, 0, 0): 1,
+    (1, 0, 1, 0, 1): 1,
+    (1, 0, 0, 0, 1): 1,
+    (1, 0, 0, 1, 0): 1,
+    (1, 1, 1, 0, 0): 1,
+    (1, 1, 1, 0, 1): 1,
+    (0, 1, 0, 1, 0): 1,
+    (0, 0, 1, 1, 0): 0,
+    (1, 1, 1, 1, 1): 0,
+    (0, 1, 1, 1, 1): 0,
+    (0, 0, 0, 1, 1): 0,
+    (0, 1, 1, 0, 0): 0,
+    (0, 1, 0, 1, 1): 0,
+    (1, 0, 0, 1, 1): 0,
+    (0, 0, 1, 0, 1): 0,
+    (0, 0, 1, 1, 1): 0,
+    (0, 1, 1, 1, 0): 0,
+    (0, 1, 0, 0, 1): 0,
+    (1, 1, 0, 0, 0): 0,
+    (1, 1, 0, 1, 0): 0,
+    (1, 1, 0, 1, 1): 0,
+    (1, 1, 1, 1, 0): 0,
+    (1, 0, 1, 1, 1): 0,
+    (1, 0, 1, 1, 0): 0,
+    (1, 1, 0, 0, 1): 0,
+    (0, 1, 1, 0, 1): 0
+    }, add_rotations= False)
+
+# evolve the cellular automaton for 60 time steps
+cellular_automaton = cpl.evolve2d(cellular_automaton, timesteps=2000, neighbourhood='von Neumann',
+                                  apply_rule=ctrbl_rule, memoize='recursive')
+
+
+cpl.plot2d_animate(cellular_automaton)
